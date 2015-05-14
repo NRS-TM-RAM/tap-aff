a2ab91862eac1f48295e7366f88b8215b846d345
cnngraphics <cnngraphics@gmail.com>
2015-05-14 02:57:27 -0400

    Publishing to branch
    Publishing to branch

--- testing.php
+++ testing.php
@@ -4,4 +4,6 @@ echo "test2";
 echo "test3";
  //this is my Echo
  echo "Testing online change";
+ // this change will be published in new branch
+ //echo branching is better than publishing to master
  ?>
